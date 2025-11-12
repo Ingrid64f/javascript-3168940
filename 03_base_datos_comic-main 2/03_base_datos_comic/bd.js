858ce02396456afe1c9895151d5ebf6a1d55a1f8
Ingrid64f <ingridjaramillo848@gmail.com>
2025-11-12 17:02:54 -0500

    Add files via upload

---
+++ 03_base_datos_comic-main 2/03_base_datos_comic/bd.js
@@ -0,0 +1,42 @@
+let comic = {
+  "nombreComic": "El eco olvidado",
+  "numeroCapitulos": 3,
+  "genero": "aventura",
+  "sinopsis": "Super tower",
+  "autores": "santiago" "luis" "ingrid",
+  "year":  "2025"
+
+  [ 
+    {
+      
+      "id": "1",
+      "nombre": eco olvidado,
+      "personajes": "raizuar", "fruko", "la abuela", "reportero", "construtores"
+      "age": 17,
+      "portada": "Dan Jukes",
+      "descripcion": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+    },
+    {
+      "name": "Madame Uppercut",
+      "age": 39,
+      "secretIdentity": "Jane Wilson",
+      "powers": [
+        "Million tonne punch",
+        "Damage resistance",
+        "Superhuman reflexes"
+      ]
+    },
+    {
+      "name": "Eternal Flame",
+      "age": 1000000,
+      "secretIdentity": "Unknown",
+      "powers": [
+        "Immortality",
+        "Heat Immunity",
+        "Inferno",
+        "Teleportation",
+        "Interdimensional travel"
+      ]
+    }
+  ]
+}
---
+++ 03_base_datos_comic-main 2/bd.js
@@ -0,0 +1,32 @@
+let comic = {
+  "nombreComic": "El eco Olvidado",
+  "numeroEpisodios": "Metro City",
+  "genero": "Fantasia, drama",
+  "sinopsis": "Un joven de 17 años decide ir a un museo abandonado donde encontrar un peculiar mentor del pasado",
+  "autores": {"Santiago", "ingrid", "Luis"} ,
+  "portadaComic":
+  "year": 2025,
+  "capitulos": [
+    {
+      "id": 1,
+      "nombre": "El eso olvidado",
+      "personajes": ["Raizura", "Abuela", "Reportero", "Constructores"],
+      "portada": ".img/portada.jpg",
+      "descripcion": "Conocemos a raizura, un joven lleno de curiosidad y fascinacion por la musica decide investigar un museo de musica"
+    },
+    {
+      "id": 2,
+      "nombre": "El fantasma de la sabrosura",
+      "personajes": ["Raizura", "Fruko"],
+      "portada": ".img/portada.jpg",
+      "Descripcion": "Raizura conoce a fruko, un fantasma que decide ayudarlo a revivir su musica",
+    },
+    {
+      "id": 3,
+      "nombre": "El primer y ultimo concierto",
+      "personajes": ["Raizura", "Fruko", "Abuela", "Trabajadores", "Ciudadanos"],
+      "portada": ".img/portada.jpg",
+      "Descripcion": "Raizura decide hacer un concierto para evitar una tragedia",
+    }
+  ]
+}
